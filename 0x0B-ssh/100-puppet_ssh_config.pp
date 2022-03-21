121aea89694c748a6725c9ba9b4d8ae15d64cd25
cPicton <pictonchris2@gmail.com>
2022-03-21 13:59:46 +0300

    last commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Setting up client SSH onfiguration file so that you can connect to a server without typing a password
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
